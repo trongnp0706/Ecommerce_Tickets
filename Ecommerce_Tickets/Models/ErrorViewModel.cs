b2e047d93bc071d68bb2c689ae8df7f5b093d488
Nguyen Phuoc Trong <phuoctrong1001@gmail.com>
2022-10-24 04:07:08 +0700

    Add project files.

---
+++ Ecommerce_Tickets/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Ecommerce_Tickets.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
