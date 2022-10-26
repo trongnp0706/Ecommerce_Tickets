eadce5d98513671317f780b253fa7fa6600d837e
Nguyen Phuoc Trong <phuoctrong1001@gmail.com>
2022-10-27 02:22:40 +0700

    Add shopping cart model

---
+++ Ecommerce_Tickets/Data/Cart/ShoppingCart.cs
@@ -0,0 +1,26 @@
+﻿using Ecommerce_Tickets.Models;
+
+using Microsoft.EntityFrameworkCore;
+
+namespace Ecommerce_Tickets.Data.Cart
+{
+    public class ShoppingCart
+    {
+        public ApplicationDbContext _context { get; set; }
+
+        public string ShoppingCartId { get; set; }
+        public List<ShoppingCartItem> ShoppingCartItems { get; set; }
+
+        public ShoppingCart(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public List<ShoppingCartItem> GetShoppingCartItems()
+        {
+            return ShoppingCartItems ?? (ShoppingCartItems = _context.ShoppingCartItems.Where(n => n.ShoppingCartId == ShoppingCartId).Include(n => n.Movie).ToList());
+        }
+
+        public double GetShoppingCartTotal() => _context.ShoppingCartItems.Where(n => n.ShoppingCartId == ShoppingCartId).Select(n => n.Movie.Price * n.Amount).Sum();
+    }
+}
