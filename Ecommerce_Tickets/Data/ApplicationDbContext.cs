dec71841e4891e0fececd9521225cc52633a0ae9
Nguyen Phuoc Trong <phuoctrong1001@gmail.com>
2022-10-27 01:21:32 +0700

    Create tables order and order items in db

--- Ecommerce_Tickets/Data/ApplicationDbContext.cs
+++ Ecommerce_Tickets/Data/ApplicationDbContext.cs
@@ -32,5 +32,9 @@ namespace Ecommerce_Tickets.Data
         public DbSet<Actor_Movie> Actors_Movies { get; set; }
         public DbSet<Cinema> Cinemas { get; set; }
         public DbSet<Producer> Producers { get; set; }
+
+        //Orders related tables
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<OrderItem> OrderItems { get; set; }
     }
 }
