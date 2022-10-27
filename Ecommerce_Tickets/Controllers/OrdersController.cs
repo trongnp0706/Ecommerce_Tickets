deebdb58fdcbb53eb7f064d7b996a6bdeb77a799
Nguyen Phuoc Trong <phuoctrong1001@gmail.com>
2022-10-28 02:14:13 +0700

    Add orders controller

---
+++ Ecommerce_Tickets/Controllers/OrdersController.cs
@@ -0,0 +1,34 @@
+﻿using Ecommerce_Tickets.Data.Cart;
+using Ecommerce_Tickets.Data.Services;
+using Ecommerce_Tickets.Data.ViewModels;
+
+using Microsoft.AspNetCore.Mvc;
+
+namespace Ecommerce_Tickets.Controllers
+{
+    public class OrdersController : Controller
+    {
+        private readonly IMoviesService _moviesService;
+        private readonly ShoppingCart _shoppingCart;
+
+        public OrdersController(IMoviesService moviesService, ShoppingCart shoppingCart)
+        {
+            _moviesService = moviesService;
+            _shoppingCart = shoppingCart;
+        }
+
+        public IActionResult Index()
+        {
+            var items = _shoppingCart.GetShoppingCartItems();
+            _shoppingCart.ShoppingCartItems = items;
+
+            var response = new ShoppingCartVM()
+            {
+                ShoppingCart = _shoppingCart,
+                ShoppingCartTotal = _shoppingCart.GetShoppingCartTotal()
+            };
+
+            return View(response);
+        }
+    }
+}
---
+++ Ecommerce_Tickets/Data/ViewModels/ShoppingCartVM.cs
@@ -0,0 +1,10 @@
+﻿using Ecommerce_Tickets.Data.Cart;
+
+namespace Ecommerce_Tickets.Data.ViewModels
+{
+    public class ShoppingCartVM
+    {
+        public ShoppingCart? ShoppingCart { get; set; }
+        public double ShoppingCartTotal { get; set; }
+    }
+}
