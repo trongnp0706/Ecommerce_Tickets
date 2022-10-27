6ee16b6eb4d06b7bac586c9bb0816f650991eb43
Nguyen Phuoc Trong <phuoctrong1001@gmail.com>
2022-10-28 03:22:18 +0700

    Add items to shopping cart

--- Ecommerce_Tickets/Controllers/OrdersController.cs
+++ Ecommerce_Tickets/Controllers/OrdersController.cs
@@ -30,5 +30,16 @@ namespace Ecommerce_Tickets.Controllers
 
             return View(response);
         }
+
+        public async Task<RedirectToActionResult> AddItemToShoppingCart(int id)
+        {
+            var item = await _moviesService.GetMovieByIdAsync(id);
+
+            if (item != null)
+            {
+                _shoppingCart.AddItemToCart(item);
+            }
+            return RedirectToAction(nameof(ShoppingCart));
+        }
     }
 }
