7f7673ce9899d523501d4885b946f3af8b22183d
Nguyen Phuoc Trong <phuoctrong1001@gmail.com>
2022-10-29 02:54:53 +0700

    Add profile section

--- Ecommerce_Tickets/Controllers/OrdersController.cs
+++ Ecommerce_Tickets/Controllers/OrdersController.cs
@@ -19,6 +19,14 @@ namespace Ecommerce_Tickets.Controllers
             _ordersService = ordersService;
         }
 
+        public async Task<IActionResult> Index()
+        {
+            string userId = "";
+
+            var orders = await _ordersService.GetOrdersByUserIdAsync(userId);
+            return View(orders);
+        }
+
         public IActionResult ShoppingCart()
         {
             var items = _shoppingCart.GetShoppingCartItems();
