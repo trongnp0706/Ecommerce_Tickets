a38de05038c86978d8fd0ae912dd60a60aa43979
Nguyen Phuoc Trong <phuoctrong1001@gmail.com>
2022-10-29 00:33:29 +0700

    Add and configured ordersservice

---
+++ Ecommerce_Tickets/Data/Services/IOrdersService.cs
@@ -0,0 +1,10 @@
+﻿using Ecommerce_Tickets.Models;
+
+namespace Ecommerce_Tickets.Data.Services
+{
+    public interface IOrdersService
+    {
+        Task StoreOrderAsync(List<ShoppingCartItem> items, string userId, string userEmailAddress);
+        Task<List<Order>> GetOrdersByUserIdAsync(string userId);
+    }
+}
